PrettyBabyMiki/woocommerce-site

commit 49708c7b8ba5a50044f35e448268d50e0ef02bec

Make quantity selector more specific Only select the primary quantity (#36087)
* Add changelog

* Use the input name when selecting the quantity field.

* Changelog tweak.

Co-authored-by: Barry Hughes <3594411+barryhughes@users.noreply.github.com>

plugins/woocommerce/client/legacy/js/frontend/add-to-cart-variation.js
@@ -245,7 +245,7 @@
 			$dimensions    = form.$product.find(
 				'.product_dimensions, .woocommerce-product-attributes-item--dimensions .woocommerce-product-attributes-item__value'
 			),
-			$qty           = form.$singleVariationWrap.find( '.quantity' ),
+			$qty           = form.$singleVariationWrap.find( '.quantity input[name="quantity"]' ),
 			purchasable    = true,
 			variation_id   = '',
 			template       = false,

commit 355e5024e116a8714208a4dd6dbd73cd65d9fd60

Fix selectors for quantity element in variable products (#36871)
Fixes bug introduced in PR #36087 that changes jQuery selectors for quantity input in variable products.

The previous logic is reinstated so that $qty points to the div wrapper, and $qty_input points to the input element inside.

Otherwise, as is in the current version, $qty_input is undefined and the min/max attributes are not updated.

This PR also addresses the original issue #36086 by restricting the selector of the div wrapper to only be the one containing input[name=quantity] (the .qty class filter is also kept to avoid any potential malfunctions of existing code).

plugins/woocommerce/client/legacy/js/frontend/add-to-cart-variation.js
@@ -263,7 +263,8 @@
 			$dimensions    = form.$product.find(
 				'.product_dimensions, .woocommerce-product-attributes-item--dimensions .woocommerce-product-attributes-item__value'
 			),
-			$qty           = form.$singleVariationWrap.find( '.quantity input[name="quantity"]' ),
+			$qty_input     = form.$singleVariationWrap.find( '.quantity input.qty[name="quantity"]' ),
+			$qty           = $qty_input.closest( '.quantity' ),
 			purchasable    = true,
 			variation_id   = '',
 			template       = false,
@@ -308,12 +309,11 @@
 
 		// Hide or show qty input
 		if ( variation.is_sold_individually === 'yes' ) {
-			$qty.find( 'input.qty' ).val( '1' ).attr( 'min', '1' ).attr( 'max', '' ).trigger( 'change' );
+			$qty_input.val( '1' ).attr( 'min', '1' ).attr( 'max', '' ).trigger( 'change' );
 			$qty.hide();
 		} else {
 
-			var $qty_input = $qty.find( 'input.qty' ),
-				qty_val    = parseFloat( $qty_input.val() );
+			var qty_val    = parseFloat( $qty_input.val() );
 
 			if ( isNaN( qty_val ) ) {
 				qty_val = variation.min_qty;